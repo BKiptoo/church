27a1ff0ce7ccea790455e46837acc5d9f5fdf90f
dev-techguy <dev.techguy@gmail.com>
2023-07-10 12:33:31 +0300

    :package: Updating compiled files or packages.

--- app/Http/Livewire/User/Slider/ListSlides.php
+++ app/Http/Livewire/User/Slider/ListSlides.php
@@ -98,6 +98,7 @@ class ListSlides extends Component
                         $query->orWhere('buttonUrl', 'ilike', '%' . $this->search . '%')
                             ->orWhere('buttonName', 'ilike', '%' . $this->search . '%')
                             ->orWhere('description', 'ilike', '%' . $this->search . '%')
+                            ->whereRelation('country', 'slug', 'ilike', '%' . $this->search . '%')
                             ->whereRelation('country', 'name', 'ilike', '%' . $this->search . '%');
                     })
                     ->paginate(10)
