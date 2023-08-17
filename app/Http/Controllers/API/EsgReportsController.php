bf525bdcbe3eb6deb3fc81beca2ee8148e63d979
dev-techguy <dev.techguy@gmail.com>
2023-08-17 17:55:43 +0300

    :construction: Added Esg endpoint

---
+++ app/Http/Controllers/API/EsgReportsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Esg;
+use App\Models\Event;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class EsgReportsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @return JsonResponse
+     */
+    public function __invoke(): JsonResponse
+    {
+        return $this->successResponse(
+            Esg::query()
+                ->with([
+                    'country',
+                    'media'
+                ])
+                ->latest()
+                ->get()
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\API\CommentController;
 use App\Http\Controllers\API\ContactController;
 use App\Http\Controllers\API\CountryController;
 use App\Http\Controllers\API\CoverageController;
+use App\Http\Controllers\API\EsgReportsController;
 use App\Http\Controllers\API\EventController;
 use App\Http\Controllers\API\FaqController;
 use App\Http\Controllers\API\FeaturedPostController;
@@ -66,6 +67,7 @@ Route::group([
     Route::get('teams/{limit?}/{countryId?}', TeamController::class);
     Route::get('tenders/{limit?}/{countryId?}', TenderController::class);
     Route::get('impact-types', ImpactTypeController::class);
+    Route::get('esg-reports', EsgReportsController::class);
     Route::get('featured-posts/{limit?}/{countryId?}', FeaturedPostController::class);
 
     // products
